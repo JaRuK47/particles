2a274b62235df4824755f8e38518bd40ad7986a7
KuRaJ <Kuraj.com>
2026-04-14 01:29:14 +0800

    second part

--- Particles/Form1.Designer.cs
+++ Particles/Form1.Designer.cs
@@ -31,7 +31,17 @@
             this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbDirection = new System.Windows.Forms.TrackBar();
+            this.lblDirection = new System.Windows.Forms.Label();
+            this.tbSpreading = new System.Windows.Forms.TrackBar();
+            this.lblSpreading = new System.Windows.Forms.Label();
+            this.tbGraviton1 = new System.Windows.Forms.TrackBar();
+            this.tbGraviton2 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpreading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGraviton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGraviton2)).BeginInit();
             this.SuspendLayout();
             // 
             // picDisplay
@@ -49,16 +59,79 @@
             this.timer1.Interval = 40;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // tbDirection
+            // 
+            this.tbDirection.Location = new System.Drawing.Point(12, 471);
+            this.tbDirection.Maximum = 359;
+            this.tbDirection.Name = "tbDirection";
+            this.tbDirection.Size = new System.Drawing.Size(291, 56);
+            this.tbDirection.TabIndex = 1;
+            this.tbDirection.Scroll += new System.EventHandler(this.tbDirection_Scroll);
+            // 
+            // lblDirection
+            // 
+            this.lblDirection.AutoSize = true;
+            this.lblDirection.Location = new System.Drawing.Point(324, 471);
+            this.lblDirection.Name = "lblDirection";
+            this.lblDirection.Size = new System.Drawing.Size(0, 16);
+            this.lblDirection.TabIndex = 3;
+            // 
+            // tbSpreading
+            // 
+            this.tbSpreading.Location = new System.Drawing.Point(366, 471);
+            this.tbSpreading.Maximum = 359;
+            this.tbSpreading.Name = "tbSpreading";
+            this.tbSpreading.Size = new System.Drawing.Size(311, 56);
+            this.tbSpreading.TabIndex = 4;
+            this.tbSpreading.Scroll += new System.EventHandler(this.tbSpreading_Scroll);
+            // 
+            // lblSpreading
+            // 
+            this.lblSpreading.AutoSize = true;
+            this.lblSpreading.Location = new System.Drawing.Point(684, 466);
+            this.lblSpreading.Name = "lblSpreading";
+            this.lblSpreading.Size = new System.Drawing.Size(0, 16);
+            this.lblSpreading.TabIndex = 5;
+            // 
+            // tbGraviton1
+            // 
+            this.tbGraviton1.Location = new System.Drawing.Point(12, 533);
+            this.tbGraviton1.Maximum = 100;
+            this.tbGraviton1.Name = "tbGraviton1";
+            this.tbGraviton1.Size = new System.Drawing.Size(291, 56);
+            this.tbGraviton1.TabIndex = 6;
+            this.tbGraviton1.Scroll += new System.EventHandler(this.tbGraviton_Scroll);
+            // 
+            // tbGraviton2
+            // 
+            this.tbGraviton2.Location = new System.Drawing.Point(366, 533);
+            this.tbGraviton2.Maximum = 100;
+            this.tbGraviton2.Name = "tbGraviton2";
+            this.tbGraviton2.Size = new System.Drawing.Size(311, 56);
+            this.tbGraviton2.TabIndex = 7;
+            this.tbGraviton2.Scroll += new System.EventHandler(this.tbGraviton2_Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 471);
+            this.ClientSize = new System.Drawing.Size(830, 623);
+            this.Controls.Add(this.tbGraviton2);
+            this.Controls.Add(this.tbGraviton1);
+            this.Controls.Add(this.lblSpreading);
+            this.Controls.Add(this.tbSpreading);
+            this.Controls.Add(this.lblDirection);
+            this.Controls.Add(this.tbDirection);
             this.Controls.Add(this.picDisplay);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpreading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGraviton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGraviton2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +139,12 @@
 
         private System.Windows.Forms.PictureBox picDisplay;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar tbDirection;
+        private System.Windows.Forms.Label lblDirection;
+        private System.Windows.Forms.TrackBar tbSpreading;
+        private System.Windows.Forms.Label lblSpreading;
+        private System.Windows.Forms.TrackBar tbGraviton1;
+        private System.Windows.Forms.TrackBar tbGraviton2;
     }
 }
 
--- Particles/Form1.cs
+++ Particles/Form1.cs
@@ -12,7 +12,11 @@ namespace Particles
 {
     public partial class Form1 : Form
     {
+        List<Emitter> emitters = new List<Emitter>();
         Emitter emitter;
+
+        GravityPoint point1; // добавил поле под первую точку
+        GravityPoint point2;
 
         public Form1()
         {
@@ -21,29 +25,58 @@ namespace Particles
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
 
-            emitter = new TopEmitter
+            this.emitter = new Emitter // создаю эмиттер и привязываю его к полю emitter
             {
-                Width = picDisplay.Width,
-                GravitationY = 0.25f
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.FromArgb(0, Color.Red),
+                ParticlesPerTick = 10,
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2,
             };
 
-            emitter.impactPoints.Add(new GravityPoint
-            {
-                X = (float)(picDisplay.Width * 0.25),
-                Y = picDisplay.Height / 2
-            });
+            emitters.Add(this.emitter);
 
-            emitter.impactPoints.Add(new AntiGravityPoint
+            point1 = new GravityPoint
             {
-                X = picDisplay.Width / 2,
-                Y = picDisplay.Height / 2
-            });
+                X = picDisplay.Width / 2 + 100,
+                Y = picDisplay.Height / 2,
+            };
+            point2 = new GravityPoint
+            {
+                X = picDisplay.Width / 2 - 100,
+                Y = picDisplay.Height / 2,
+            };
 
-            emitter.impactPoints.Add(new GravityPoint
-            {
-                X = (float)(picDisplay.Width * 0.75),
-                Y = picDisplay.Height / 2
-            });
+            emitter.impactPoints.Add(point1);
+            emitter.impactPoints.Add(point2);
+
+            //emitter = new TopEmitter
+            //{
+            //    Width = picDisplay.Width,
+            //    GravitationY = 0.25f
+            //};
+
+            //emitter.impactPoints.Add(new GravityPoint
+            //{
+            //    X = (float)(picDisplay.Width * 0.25),
+            //    Y = picDisplay.Height / 2
+            //});
+
+            //emitter.impactPoints.Add(new AntiGravityPoint
+            //{
+            //    X = picDisplay.Width / 2,
+            //    Y = picDisplay.Height / 2
+            //});
+
+            //emitter.impactPoints.Add(new GravityPoint
+            //{
+            //    X = (float)(picDisplay.Width * 0.75),
+            //    Y = picDisplay.Height / 2
+            //});
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -60,8 +93,37 @@ namespace Particles
 
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            emitter.MousePositionX = e.X;
-            emitter.MousePositionY = e.Y;
+            foreach (var emitter in emitters)
+            {
+                emitter.MousePositionX = e.X;
+                emitter.MousePositionY = e.Y;
+            }
+
+            // а тут передаем положение мыши, в положение гравитона
+            point2.X = e.X;
+            point2.Y = e.Y;
+        }
+
+        private void tbDirection_Scroll(object sender, EventArgs e)
+        {
+            emitter.Direction = tbDirection.Value;
+            lblDirection.Text = $"{tbDirection.Value}°";
+        }
+
+        private void tbSpreading_Scroll(object sender, EventArgs e)
+        {
+            emitter.Spreading = tbSpreading.Value;
+            lblSpreading.Text = $"{tbSpreading.Value}°";
+        }
+
+        private void tbGraviton_Scroll(object sender, EventArgs e)
+        {
+            point2.Power = tbGraviton1.Value;
+        }
+
+        private void tbGraviton2_Scroll(object sender, EventArgs e)
+        {
+            point1.Power = tbGraviton2.Value;
         }
     }
 }
